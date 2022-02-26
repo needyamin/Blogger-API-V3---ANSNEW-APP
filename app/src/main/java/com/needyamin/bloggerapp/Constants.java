cb7ef30d46aed2ab2c9af318394954a61d2c7f6d
Md. Yamin Hossain <needyamin@gmail.com>
2022-02-27 04:07:51 +0600

    Update Constants.java

--- app/src/main/java/com/needyamin/bloggerapp/Constants.java
+++ app/src/main/java/com/needyamin/bloggerapp/Constants.java
@@ -3,8 +3,8 @@ package com.needyamin.bloggerapp;
 public class Constants {
 
 
-    public static final String API_KEY ="AIzaSyBGsAJDGeF9LzHrrXo0iWU39uLsRs1pnVM";
-    public static final String BLOG_ID="3422137415075355570";
+    public static final String API_KEY ="******************************";
+    public static final String BLOG_ID="*************";
     public static final  String MAX_POST_RESULTS = "10";
 }
 
